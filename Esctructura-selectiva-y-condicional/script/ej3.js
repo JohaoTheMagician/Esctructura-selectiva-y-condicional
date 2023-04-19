49634b7a4806f982de551d1add5c0448211ede80
JohaoTheMagician <127440675+JohaoTheMagician@users.noreply.github.com>
2023-04-19 16:26:50 -0500

    Add files via upload

---
+++ Esctructura-selectiva-y-condicional/script/ej3.js
@@ -0,0 +1,23 @@
+let nombre, nombre2, salario, salario2, sal1, sal2, 
+
+nombre = parseInt(prompt('ingrese su nombre'))
+nombre2 = parseInt(prompt('ingrese su nombre'))
+salario = parseInt(prompt('ingrese su salario'+ nombre))
+salario2 = parseInt(prompt('ingrese su salario'+ nombre2))
+sal1 = parseInt(prompt('ingrese sus deducciones')+ nombre)
+sal2 = parseInt(prompt('ingrese sus deducciones')+ nombre2)
+
+
+salario = salario - sal1 + 100000
+
+salario2 = salario2 - sal2 + 100000
+
+//proceso
+if (salario<salario2) {
+    alert ('ae')
+}
+
+
+
+
+
---
+++ Esctructura-selectiva-y-condicional/script/ejercicio2.js
@@ -0,0 +1,37 @@
+let edad
+
+//leer informacion
+edad = parseInt(prompt('ingrese su edad'))
+
+
+//proceso
+if (edad<10 ) {
+    alert('niño')
+} else if (edad>=10 && edad<15) {
+    alert('pre-adolecente')
+} else if(edad>=15 && edad<18) {
+    alert('adolecente')
+} else if (edad>=18 && edad<50) {
+    alert('mayor')
+} else if (edad>=50) {
+    alert('adulto mayor')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
